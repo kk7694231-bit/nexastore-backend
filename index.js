85f7525e1f6ef2c536c92fa7831e4b36eacce0c5
vinsupkishore <kk7694231@gmail.com>
2026-06-18 20:08:22 +0530

    Fix MongoDB connection for Vercel

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./config/db.js";
+
 import authRoutes from "./routes/authRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import adminRoutes from "./routes/adminRoutes.js";
@@ -10,41 +11,40 @@ import cartRoutes from "./routes/cartRoutes.js";
 import wishlistRoutes from "./routes/wishlistRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
 import reviewRoutes from "./routes/reviewRoutes.js";
-import mongoose from "mongoose";
-const PORT = 5000;
-const app = express();
-app.use(express.json());
-app.use(cors());
+
 dotenv.config();
-app.listen(PORT, () => {
-  console.log(
-    `Server Running On Port ${PORT}`
-  );
-});
 
+// Connect MongoDB
+connectDB();
 
+const app = express();
 
+// Middleware
+app.use(cors());
+app.use(express.json());
 
-
+// Test Route
 app.get("/", (req, res) => {
   res.send("Amazon Clone Backend Running");
 });
 
-mongoose.connect("mongodb://vinsupkishore:vinsupkishore@ac-cimxs3u-shard-00-00.bwcy5qr.mongodb.net:27017,ac-cimxs3u-shard-00-01.bwcy5qr.mongodb.net:27017,ac-cimxs3u-shard-00-02.bwcy5qr.mongodb.net:27017/?ssl=true&replicaSet=atlas-ni0tdb-shard-0&authSource=admin&appName=Cluster0")
-.then(()=>{
-  console.log("DB Connected");
-})
-
-.catch((err)=>{
-  console.log(err);
-})
-
-
+// Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
-app.use("/api/admin", adminRoutes); 
+app.use("/api/admin", adminRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/wishlist", wishlistRoutes);
 app.use("/api/orders", orderRoutes);
 app.use("/api/reviews", reviewRoutes);
+
+// Local Development Only
+const PORT = process.env.PORT || 5000;
+
+if (process.env.NODE_ENV !== "production") {
+  app.listen(PORT, () => {
+    console.log(`Server Running On Port ${PORT}`);
+  });
+}
+
+export default app;
