e489a8539542befc3d19ef316583e7920008e059
vinsupkishore <kk7694231@gmail.com>
2026-06-19 12:21:52 +0530

    test startup

--- controllers/authController.js
+++ controllers/authController.js
@@ -5,58 +5,43 @@ import jwt from "jsonwebtoken";
 // Register User
 export const registerUser = async (req, res) => {
   try {
-    console.log("REGISTER BODY:", req.body);
-
     const { name, email, password } = req.body;
 
     const userExists = await User.findOne({ email });
 
-    console.log("USER EXISTS:", userExists);
-
     if (userExists) {
       return res.status(400).json({
-        message: "User already exists"
+        message: "User already exists",
       });
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    console.log("PASSWORD HASHED");
-
-    const user = await User.create({
+    await User.create({
       name,
       email,
       password: hashedPassword,
-      role: "user"
+      role: "user",
     });
-
-    console.log("USER CREATED:", user);
 
     res.status(201).json({
-      message: "Registration Successful"
+      message: "Registration Successful",
     });
-
   } catch (error) {
-  console.error("REGISTER ERROR:", error);
+    console.error("REGISTER ERROR:", error);
 
-  return res.status(500).json({
-    success: false,
-    message: error.message,
-    stack: error.stack
-  });
-}
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
 
+// Login User
 export const loginUser = async (req, res) => {
-   try {
-    console.log("Request Body:", req.body);
-    console.log("MONGO_URI Exists:", !!process.env.MONGO_URI);
-    console.log("JWT_SECRET Exists:", !!process.env.JWT_SECRET);
-
+  try {
     const { email, password } = req.body;
 
     const user = await User.findOne({ email });
-
-    console.log("User Found:", user);
 
     if (!user) {
       return res.status(400).json({
@@ -64,9 +49,10 @@ export const loginUser = async (req, res) => {
       });
     }
 
-    const isMatch = await bcrypt.compare(password, user.password);
-
-    console.log("Password Match:", isMatch);
+    const isMatch = await bcrypt.compare(
+      password,
+      user.password
+    );
 
     if (!isMatch) {
       return res.status(400).json({
@@ -86,9 +72,9 @@ export const loginUser = async (req, res) => {
     });
   } catch (error) {
     console.error("LOGIN ERROR:", error);
+
     res.status(500).json({
       message: error.message,
-      stack: error.stack,
     });
   }
 };
