36fad70aadea549e7cb4fd4e15f7f3d4a1f57fd0
vinsupkishore <kk7694231@gmail.com>
2026-06-19 15:52:42 +0530

    Remove process exit

--- config/db.js
+++ config/db.js
@@ -9,7 +9,6 @@ const connectDB = async () => {
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error("DATABASE ERROR:", error);
-    process.exit(1);
   }
 };
 
